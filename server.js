429dd1d1af60d5c738a7746c1ef809f1ee3dd3bb
Afrah Mehveen <afrahmehveen@gmail.com>
2022-03-04 20:22:11 +0400

    cors

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(function(req ,res) {
     res.send("File not found!");
 });
 
-
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,static,'/static/Coursework1.html'));
